724a0e83ba58614b414951921fad326c862bab56
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-18 23:10:38 +0530

    added bcrypt

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,20 @@ const { Router } = require("express");
 const { userModel } = require("../db");
 const jwt  = require("jsonwebtoken");
 const { JWT_PASSWORD } = require("../config");
+const bcrypt = require("bcrypt");
 
 const userRouter = Router();
 
 userRouter.post("/signup",async(req,res)=>{
     const {userName,email,password} = req.body;
 
+    const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
     await userModel.create({
         userName:userName,
         email:email,
-        password:password
+        password:hashedPassword
     })
 
     res.json({
@@ -24,18 +28,26 @@ userRouter.post("/signin",async (req,res)=>{
     const {email,password} = req.body;
 
     const user = await userModel.findOne({
-        email:email,
-        password:password
+        email:email
     });
+    if(!user){
+        res.status(404).json("user not found")
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(!validPassword){
+        res.status(404).json("password is incorrect")
+    }
 
     if(user){
         const token = jwt.sign({
             id:user._id
         },JWT_PASSWORD)
-
-        res.json({
+    
+        res.status(200).json({
+            user : user,
             token : token
-        })
+        });
     }else{
         res.status(403).json({
             message:"Incorrect credentials"
